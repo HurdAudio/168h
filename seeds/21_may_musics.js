d7c3b2d0f038efb7b8f5a51b4b3b362f5346977c
Devin Hurd <hurdaudio@gmail.com>
2017-08-29 15:49:52 -0400

    updated seeds on may_musics file

--- seeds/21_may_musics.js
+++ seeds/21_may_musics.js
@@ -58,6 +58,60 @@ exports.seed = function(knex, Promise) {
           },
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 4,
+          user_id: 1,
+          theme: 'avant garde/new music',
+          source: 'bandcamp',
+          src_string: 'https://bandcamp.com/EmbeddedPlayer/album=2824493515/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/',
+          href_string: 'http://zeitkratzer.bandcamp.com/album/karlheinz-stockhausen-old-school',
+          a_string: 'Karlheinz Stockhausen [old school] by zeitkratzer',
+          rule: {
+            "monday": "[ 2, 8, 15, 28 ]",
+            "tuesday": "[ 1, 7, 14, 27 ]",
+            "wednesday": "[ 5, 11, 18, 31 ]",
+            "thursday": "[ 4, 10, 17, 30 ]",
+            "saturday": "[ 3, 9, 16, 29 ]"
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 5,
+          user_id: 1,
+          theme: 'avant garde/new music',
+          source: 'bandcamp',
+          src_string: 'https://bandcamp.com/EmbeddedPlayer/album=1635773245/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/',
+          href_string: 'http://idmtheftable.bandcamp.com/album/leaving-the-keyboard-that-plays-itself-down-by-the-river-between-2-30-and-3-30-am-august-3rd-2017',
+          a_string: 'Leaving the keyboard that plays itself down by the river between 2:30 and 3:30 AM August 3rd 2017 by id m theft able',
+          rule: {
+            "monday": "[ 3, 9, 16, 29 ]",
+            "tuesday": "[ 2, 8, 15, 28 ]",
+            "wednesday": "[ 1, 7, 14, 27 ]",
+            "thursday": "[ 5, 11, 18, 31 ]",
+            "saturday": "[ 4, 10, 17, 30 ]"
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 6,
+          user_id: 1,
+          theme: 'avant garde/new music',
+          source: 'bandcamp',
+          src_string: 'https://bandcamp.com/EmbeddedPlayer/album=1468997469/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/',
+          href_string: 'http://babel-label.bandcamp.com/album/karlheinz-stockhausen-tierkreis',
+          a_string: 'KARLHEINZ STOCKHAUSEN TIERKREIS by BRUNO HEINEN SEXTET',
+          rule: {
+            "monday": "[ 4, 10, 17, 30 ]",
+            "tuesday": "[ 3, 9, 16, 29 ]",
+            "wednesday": "[ 2, 8, 15, 28 ]",
+            "thursday": "[ 1, 7, 14, 27 ]",
+            "saturday": "[ 5, 11, 18, 31 ]"
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
