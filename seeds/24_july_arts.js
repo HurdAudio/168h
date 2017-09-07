641b391776014bb45efb2c676fccb03a9f0acf40
Devin Hurd <hurdaudio@gmail.com>
2017-09-07 15:07:43 -0400

    july arts seeds updated

--- seeds/24_july_arts.js
+++ seeds/24_july_arts.js
@@ -204,6 +204,106 @@ exports.seed = function(knex, Promise) {
           },
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 11,
+          user_id: 1,
+          theme: 'french revolution',
+          img_path: './img/july/.jpg',
+          title: 'place-de-la-bastille-2',
+          artist: 'Edouard Cortes',
+          year: 'XIX-XX cent.',
+          rule: {
+            "monday": [ 30, 10, 21, 1 ],
+            "tuesday": [ 29, 9, 20, 31 ],
+            "wednesday": [ 4, 15, 26, 6 ],
+            "thursday": [ 3, 14, 25, 5 ],
+            "friday": [ 2, 13, 24, 4 ],
+            "saturday": [ 1, 12, 23, 3 ],
+            "sunday": [ 31, 11, 22, 2 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 12,
+          user_id: 1,
+          theme: 'french revolution',
+          img_path: './img/july/place-de-la-bastille-6.jpg',
+          title: 'Place de la Bastille',
+          artist: 'Edouard Cortes',
+          year: 'XIX-XX cent.',
+          rule: {
+            "monday": [ 31, 11, 22, 2 ],
+            "tuesday": [ 30, 10, 21, 1 ],
+            "wednesday": [ 29, 9, 20, 31 ],
+            "thursday": [ 4, 15, 26, 6 ],
+            "friday": [ 3, 14, 25, 5 ],
+            "saturday": [ 2, 13, 24, 4 ],
+            "sunday": [ 1, 12, 23, 3 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 13,
+          user_id: 1,
+          theme: 'french revolution',
+          img_path: './img/july/la-place-de-la-bastille-paris.jpg',
+          title: 'La Place de la Bastille, Paris',
+          artist: 'Edouard Cortes',
+          year: 'XIX-XX cent.',
+          rule: {
+            "monday": [ 1, 12, 23, 3 ],
+            "tuesday": [ 31, 11, 22, 2 ],
+            "wednesday": [ 30, 10, 21, 1 ],
+            "thursday": [ 29, 9, 20, 31 ],
+            "friday": [ 4, 15, 26, 6 ],
+            "saturday": [ 3, 14, 25, 5 ],
+            "sunday": [ 2, 13, 24, 4 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 14,
+          user_id: 1,
+          theme: 'french revolution',
+          img_path: './img/july/place-de-la-bastille-33.jpg',
+          title: 'Place de la Bastille',
+          artist: 'Edouard Cortes',
+          year: 'XIX-XX cent.',
+          rule: {
+            "monday": [ 3, 14, 25, 5 ],
+            "tuesday": [ 2, 13, 24, 4 ],
+            "wednesday": [ 1, 12, 23, 3 ],
+            "thursday": [ 31, 11, 22, 2 ],
+            "friday": [ 30, 10, 21, 1 ],
+            "saturday": [ 5, 16, 27, 7 ],
+            "sunday": [ 4, 15, 26, 6 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 15,
+          user_id: 1,
+          theme: 'french revolution',
+          img_path: './img/july/place-de-la-bastille-4.jpg',
+          title: 'Place de la Bastille',
+          artist: 'Edouard Cortes',
+          year: 'XIX-XX cent.',
+          rule: {
+            "monday": [ 4, 15, 26, 6 ],
+            "tuesday": [ 3, 14, 25, 5 ],
+            "wednesday": [ 2, 13, 24, 4 ],
+            "thursday": [ 1, 12, 23, 3 ],
+            "friday": [ 31, 11, 22, 2 ],
+            "saturday": [ 30, 10, 21, 1 ],
+            "sunday": [ 5, 16, 27, 7 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
