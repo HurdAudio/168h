41df0917cf3c0b2351a708c723ba10219575e7ba
Devin Hurd <hurdaudio@gmail.com>
2017-09-11 12:16:52 -0400

    update august musics seeds

--- seeds/27_august_musics.js
+++ seeds/27_august_musics.js
@@ -58,6 +58,60 @@ exports.seed = function(knex, Promise) {
           },
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 4,
+          user_id: 1,
+          theme: 'noise/non-idiomatic',
+          source: 'bandcamp',
+          src_string: 'https://bandcamp.com/EmbeddedPlayer/album=309991513/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/',
+          href_string: 'http://chondriticsound.bandcamp.com/album/trojan-self-image',
+          a_string: 'Trojan Self Image by Shredded Nerve',
+          rule: {
+            "monday": [ 5, 13, 29, 28 ],
+            "tuesday": [ 4, 12, 28, 27 ],
+            "wednesday": [ 8, 16, 1, 2 ],
+            "thursday": [ 7, 15, 31, 30 ],
+            "saturday": [ 6, 14, 30, 29 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 5,
+          user_id: 1,
+          theme: 'noise/non-idiomatic',
+          source: 'bandcamp',
+          src_string: 'https://bandcamp.com/EmbeddedPlayer/album=2992995276/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/',
+          href_string: 'http://redvenicerecords.bandcamp.com/album/rvr0156-torturing-nurse-live-dvd',
+          a_string: '[RVR0156] Torturing Nurse Live DVD by Torturing Nurse',
+          rule: {
+            "monday": [ 6, 14, 30, 29 ],
+            "tuesday": [ 5, 13, 29, 28 ],
+            "wednesday": [ 4, 12, 28, 27 ],
+            "thursday": [ 8, 16, 1, 2 ],
+            "saturday": [ 7, 15, 31, 30 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 6,
+          user_id: 1,
+          theme: 'noise/non-idiomatic',
+          source: 'bandcamp',
+          src_string: 'https://bandcamp.com/EmbeddedPlayer/album=1422793729/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/',
+          href_string: 'http://clppng.bandcamp.com/album/splendor-misery',
+          a_string: 'Splendor &amp; Misery by clipping.',
+          rule: {
+            "monday": [ 7, 15, 31, 30 ],
+            "tuesday": [ 6, 14, 30, 29 ],
+            "wednesday": [ 5, 13, 29, 28 ],
+            "thursday": [ 4, 12, 28, 27 ],
+            "saturday": [ 8, 16, 1, 2 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
