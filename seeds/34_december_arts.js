789e8333427126f9f99c3b6ecfe1b6ebcb9988b5
Devin Hurd <hurdaudio@gmail.com>
2017-09-26 14:34:15 -0400

    added december arts table

---
+++ seeds/34_december_arts.js
@@ -0,0 +1,132 @@
+'use strict';
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('december_arts').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('december_arts').insert([
+        {
+          id: 1,
+          user_id: 1,
+          theme: 'pulp',
+          img_path: './img/december/44-04,Suspense.jpg',
+          title: 'Suspense Comics',
+          artist: 'Nina Albright',
+          year: '1944',
+          rule: {
+            "monday": [ 3, 16, 24, 30 ],
+            "tuesday": [ 2, 15, 23, 29 ],
+            "wednesday": [ 1, 14, 22, 28 ],
+            "thursday": [ 31, 13, 21, 27 ],
+            "friday": [ 30, 12, 20, 26 ],
+            "saturday": [ 29, 11, 19, 25 ],
+            "sunday": [ 4, 17, 25, 31 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 2,
+          user_id: 1,
+          theme: 'pulp',
+          img_path: './img/december/34-00,The NatlGrdsman.jpg',
+          title: 'National Guardsman',
+          artist: 'Newton H. Alfred',
+          year: '1934',
+          rule: {
+            "monday": [ 4, 17, 25, 31 ],
+            "tuesday": [ 3, 16, 24, 30 ],
+            "wednesday": [ 2, 15, 23, 29 ],
+            "thursday": [ 1, 14, 22, 28 ],
+            "friday": [ 31, 13, 21, 27 ],
+            "saturday": [ 30, 12, 20, 26 ],
+            "sunday": [ 29, 11, 19, 25 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 3,
+          user_id: 1,
+          theme: 'pulp',
+          img_path: './img/december/17-00,Maupassant.jpg',
+          title: 'Book Illustration',
+          artist: 'James E. Allen',
+          year: '1917',
+          rule: {
+            "monday": [ 29, 11, 19, 25 ],
+            "tuesday": [ 4, 17, 25, 31 ],
+            "wednesday": [ 3, 16, 24, 30 ],
+            "thursday": [ 2, 15, 23, 29 ],
+            "friday": [ 1, 14, 22, 28 ],
+            "saturday": [ 31, 13, 21, 27 ],
+            "sunday": [ 30, 12, 20, 26 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 4,
+          user_id: 1,
+          theme: 'pulp',
+          img_path: './img/december/06-06,MotorBoys.jpg',
+          title: 'The Motor Boys',
+          artist: 'W. M. Allison',
+          year: '1906',
+          rule: {
+            "monday": [ 30, 12, 20, 26 ],
+            "tuesday": [ 29, 11, 19, 25 ],
+            "wednesday": [ 4, 17, 25, 31 ],
+            "thursday": [ 3, 16, 24, 30 ],
+            "friday": [ 2, 15, 23, 29 ],
+            "saturday": [ 1, 14, 22, 28 ],
+            "sunday": [ 31, 13, 21, 27 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 5,
+          user_id: 1,
+          theme: 'pulp',
+          img_path: './img/december/1941-04,SpicyMystery.jpg',
+          title: 'Spicy Mystery',
+          artist: 'Allen Anderson',
+          year: '1941',
+          rule: {
+            "monday": [ 31, 13, 21, 27 ],
+            "tuesday": [ 30, 12, 20, 26 ],
+            "wednesday": [ 29, 11, 19, 25 ],
+            "thursday": [ 4, 17, 25, 31 ],
+            "friday": [ 3, 16, 24, 30 ],
+            "saturday": [ 2, 15, 23, 29 ],
+            "sunday": [ 1, 14, 22, 28 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 6,
+          user_id: 1,
+          theme: 'pulp',
+          img_path: './img/december/21-07,TheAmer1.jpg',
+          title: 'The American',
+          artist: 'Frederic A. Anderson',
+          year: '1921',
+          rule: {
+            "monday": [ 1, 14, 22, 28 ],
+            "tuesday": [ 31, 13, 21, 27 ],
+            "wednesday": [ 30, 12, 20, 26 ],
+            "thursday": [ 29, 11, 19, 25 ],
+            "friday": [ 4, 17, 25, 31 ],
+            "saturday": [ 3, 16, 24, 30 ],
+            "sunday": [ 2, 15, 23, 29 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        }
+      ]);
+    }).then(() => {
+      return knex.raw("SELECT setval('december_arts_id_seq', (SELECT MAX(id) FROM december_arts));");
+    });
+};
