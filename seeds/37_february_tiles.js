38847890c8d1b3b5f92c75f4566a4ec16af534dd
Devin Hurd <hurdaudio@gmail.com>
2017-10-02 06:50:39 -0400

    added february tiles table

---
+++ seeds/37_february_tiles.js
@@ -0,0 +1,26 @@
+'use strict';
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('february_tiles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('february_tiles').insert([
+        {
+          id: 1,
+          user_id: 1,
+          theme: 'ice',
+          type: 'default',
+          src_string: '../img/february/Red-Silk-Fabric-Texture-1.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '200%',
+          color_dark: '#76081B',
+          color_medium: '#CD2C3A',
+          color_light: '#E8526B',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        }
+      ]);
+    }).then(() => {
+      return knex.raw("SELECT setval('february_tiles_id_seq', (SELECT MAX(id) FROM february_tiles));");
+    });
+};
