73c5ea95e3ab333e42cbd4867fd5e81d33b483a7
Devin Hurd <hurdaudio@gmail.com>
2017-10-20 20:54:42 -0600

    added september tiles table

---
+++ seeds/44_september_tiles.js
@@ -0,0 +1,26 @@
+'use strict';
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('september_tiles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('september_tiles').insert([
+        {
+          id: 1,
+          user_id: 1,
+          theme: 'leather',
+          type: 'default',
+          src_string: '../img/september_tiles/leatherDefault.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '165%',
+          color_dark: '#5D4940',
+          color_medium: '#8A5B4B',
+          color_light: '#8A5B4B',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        }
+      ]);
+    }).then(() => {
+      return knex.raw("SELECT setval('september_tiles_id_seq', (SELECT MAX(id) FROM september_tiles));");
+    });
+};
