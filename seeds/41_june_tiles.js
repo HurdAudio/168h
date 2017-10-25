1d460a7cf9927328ec12e2d12c3205fae83dfef8
Devin Hurd <hurdaudio@gmail.com>
2017-10-25 09:59:37 -0600

    update june tiles seeds

--- seeds/41_june_tiles.js
+++ seeds/41_june_tiles.js
@@ -60,6 +60,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FDEC7C',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 5,
+          user_id: 1,
+          theme: 'light',
+          type: 'friday',
+          src_string: '../img/june_tiles/light2.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '140%',
+          color_dark: '#010602',
+          color_medium: '#D7A057',
+          color_light: '#FFFFFF',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
