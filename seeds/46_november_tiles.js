8e91161c5e848b6ea0006e99226eff16ab1efd1e
Devin Hurd <hurdaudio@gmail.com>
2017-11-04 08:11:15 -0600

    added november tiles table

--- seeds/46_november_tiles.js
+++ seeds/46_november_tiles.js
@@ -1,26 +1,26 @@
 'use strict';
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('october_tiles').del()
+  return knex('november_tiles').del()
     .then(function () {
       // Inserts seed entries
-      return knex('october_tiles').insert([
+      return knex('november_tiles').insert([
         {
           id: 1,
           user_id: 1,
           theme: 'autumnal',
           type: 'default',
-          src_string: '../img/october_tiles/autumnalDefault.jpg.jpg',
+          src_string: '../img/november_tiles/black-and-white-images-of-birds-3-widescreen-wallpaper.jpg',
           repeat_value: 'no-repeat',
-          size_value: '140%',
-          color_dark: '#926A50',
-          color_medium: '#CA993C',
-          color_light: '#E0BC5E',
+          size_value: '130%',
+          color_dark: '#323232',
+          color_medium: '#A7A7A7',
+          color_light: '#EBEBEB',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
-      return knex.raw("SELECT setval('october_tiles_id_seq', (SELECT MAX(id) FROM october_tiles));");
+      return knex.raw("SELECT setval('november_tiles_id_seq', (SELECT MAX(id) FROM november_tiles));");
     });
 };
