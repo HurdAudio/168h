5c2533cf711484b57f4367b57ffd8e8f27f65411
Devin Hurd <hurdaudio@gmail.com>
2017-12-02 17:24:20 -0700

    update february tiles seeds

--- seeds/37_february_tiles.js
+++ seeds/37_february_tiles.js
@@ -172,6 +172,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#B08C62',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 13,
+          user_id: 1,
+          theme: 'silk',
+          type: 'week',
+          src_string: '../img/february_tiles/background-1241207_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '155%',
+          color_dark: '#180606',
+          color_medium: '#BF070C',
+          color_light: '#F6B6AE',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
