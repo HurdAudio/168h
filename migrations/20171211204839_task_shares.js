66b29121a0e00334e41097cbbcb0ff8497e2147c
Devin Hurd <hurdaudio@gmail.com>
2017-12-11 20:51:13 -0700

    added task shares table

---
+++ migrations/20171211204839_task_shares.js
@@ -0,0 +1,15 @@
+'use strict';
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('task_shares', function(table) {
+    table.increments().primary();
+    table.integer('user_id').notNullable().defaultTo(1).references('id').inTable('users').onDelete('CASCADE').index();
+    table.integer('task_id').nullable().references('id').inTable('tasks').onDelete('CASCADE').index();
+    table.integer('share_associate_id').notNullable().defaultTo(1).references('id').inTable('users').onDelete('CASCADE').index();
+    table.boolean('accepted').notNullable().defaultTo(false);
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('task_shares');
+};
