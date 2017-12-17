2e98095e2f09b474d4f678f94db7bebba1b0ab85
Devin Hurd <hurdaudio@gmail.com>
2017-12-16 19:02:33 -0700

    update april tiles seeds

--- seeds/39_april_tiles.js
+++ seeds/39_april_tiles.js
@@ -228,6 +228,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#699DC5',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 17,
+          user_id: 1,
+          theme: 'water',
+          type: 'holiday',
+          src_string: '../img/april_tiles/duck-2396740_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '150%',
+          color_dark: '#2D3D50',
+          color_medium: '#9EA4A0',
+          color_light: '#CDD2D2',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
