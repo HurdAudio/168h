8b13a0281b516f963329d5727d104604067692ae
Devin Hurd <hurdaudio@gmail.com>
2017-12-21 20:29:37 -0700

    update july tiles seeds

--- seeds/42_july_tiles.js
+++ seeds/42_july_tiles.js
@@ -228,6 +228,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FEFEFC',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 17,
+          user_id: 1,
+          theme: 'steam',
+          type: 'holiday',
+          src_string: '../img/july_tiles/rainbow_cloud_texture_by_kmiloneko.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '135%',
+          color_dark: '#8759FC',
+          color_medium: '#81FF5E',
+          color_light: '#BDFFFE',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
