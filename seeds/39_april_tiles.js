e38f94dbc153e43f00a352bcfa261da721068ce3
Devin Hurd <hurdaudio@gmail.com>
2017-12-24 21:09:24 -0700

    update april tiles seeds

--- seeds/39_april_tiles.js
+++ seeds/39_april_tiles.js
@@ -298,6 +298,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#E6FDE9',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 22,
+          user_id: 1,
+          theme: 'Water',
+          type: 'holiday',
+          src_string: '../img/april_tiles/swan-2084971_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '135%',
+          color_dark: '#020609',
+          color_medium: '#506674',
+          color_light: '#CAD1D9',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
