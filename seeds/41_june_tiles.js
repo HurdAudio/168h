823d5c373064cad00eb606a227fa4abdf678cb89
Devin Hurd <hurdaudio@gmail.com>
2017-12-26 21:31:12 -0700

    update june tiles seeds

--- seeds/41_june_tiles.js
+++ seeds/41_june_tiles.js
@@ -284,6 +284,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FBFDF8',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 21,
+          user_id: 1,
+          theme: 'Light',
+          type: 'sunday',
+          src_string: '../img/june_tiles/lighthouse-66943_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '180%',
+          color_dark: '#000000',
+          color_medium: '#0C3088',
+          color_light: '#FEFFF3',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
