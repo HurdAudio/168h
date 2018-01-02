3255abe12f06504be05291d94b0e232d041e379f
Devin Hurd <hurdaudio@gmail.com>
2018-01-01 17:38:11 -0700

    update april tiles seeds

--- seeds/39_april_tiles.js
+++ seeds/39_april_tiles.js
@@ -326,6 +326,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FFFEBE',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 24,
+          user_id: 1,
+          theme: 'Water',
+          type: 'friday',
+          src_string: '../img/april_tiles/isar-2380400_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '200%',
+          color_dark: '#31301C',
+          color_medium: '#A6998B',
+          color_light: '#D6DEE9',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
