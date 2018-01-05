4a65bab39d6e11a87cdc2eaae668a91ee25fb07d
Devin Hurd <hurdaudio@gmail.com>
2018-01-04 18:17:52 -0700

    update december tiles seeds

--- seeds/47_december_tiles.js
+++ seeds/47_december_tiles.js
@@ -242,6 +242,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#C2CBD0',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 18,
+          user_id: 1,
+          theme: 'Snow',
+          type: 'week',
+          src_string: '../img/december_tiles/winter-2435148_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '180%',
+          color_dark: '#28263E',
+          color_medium: '#4F5055',
+          color_light: '#C5C1B6',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
