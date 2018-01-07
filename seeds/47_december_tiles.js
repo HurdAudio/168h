693c56f3cc037c49f730d80affab82356e99468a
Devin Hurd <hurdaudio@gmail.com>
2018-01-07 07:44:03 -0700

    update december tiles seeds

--- seeds/47_december_tiles.js
+++ seeds/47_december_tiles.js
@@ -256,6 +256,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#C5C1B6',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 19,
+          user_id: 1,
+          theme: 'Snow',
+          type: 'friday',
+          src_string: '../img/december_tiles/lapland-2984828_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '152%',
+          color_dark: '#1D1E22',
+          color_medium: '#768CAC',
+          color_light: '#FFF9DD',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
