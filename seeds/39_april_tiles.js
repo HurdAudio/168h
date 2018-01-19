c5f9d84dfe90203c13f6783e386e07a7462dbb80
Devin Hurd <hurdaudio@gmail.com>
2018-01-19 04:12:41 -0700

    update april tiles seeds

--- seeds/39_april_tiles.js
+++ seeds/39_april_tiles.js
@@ -382,6 +382,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#4BA594',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 28,
+          user_id: 1,
+          theme: 'Water',
+          type: 'week',
+          src_string: '../img/april_tiles/seagull-677384_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '154%',
+          color_dark: '#060800',
+          color_medium: '#6A7A79',
+          color_light: '#BDCAD0',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
