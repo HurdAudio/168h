555d78d60cff2ac6b20788c1966bbd7cfef64c52
Devin Hurd <hurdaudio@gmail.com>
2018-01-18 20:12:18 -0700

    update october tiles seeds

--- seeds/45_october_tiles.js
+++ seeds/45_october_tiles.js
@@ -298,6 +298,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FFE5CC',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 22,
+          user_id: 1,
+          theme: 'Autumnal',
+          type: 'holiday',
+          src_string: '../img/october_tiles/fall-950443_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '145%',
+          color_dark: '#080108',
+          color_medium: '#C8400E',
+          color_light: '#F6EEC8',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
