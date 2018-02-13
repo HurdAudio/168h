8b9fe96ae3e9be627ad05cba736d2997dcf9c5de
Devin Hurd <hurdaudio@gmail.com>
2018-02-13 09:18:24 -0700

    update august tiles seeds

--- seeds/43_august_tiles.js
+++ seeds/43_august_tiles.js
@@ -340,6 +340,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FFFFFF',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 25,
+          user_id: 1,
+          theme: 'Primary Colors',
+          type: 'saturday',
+          src_string: '../img/august_tiles/blue-wall-texture-with-damask-design-background.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '150%',
+          color_dark: '#020114',
+          color_medium: '#010C6B',
+          color_light: '#356DF6',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
