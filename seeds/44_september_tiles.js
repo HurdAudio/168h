3313ce0230e6ecae5ff038710c5606a849c4ce7e
Devin Hurd <hurdaudio@gmail.com>
2018-03-04 07:37:56 -0700

    update september tiles seeds

--- seeds/44_september_tiles.js
+++ seeds/44_september_tiles.js
@@ -382,6 +382,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FDA720',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 28,
+          user_id: 1,
+          theme: 'Leather',
+          type: 'week',
+          src_string: '../img/september_tiles/_original.jpeg',
+          repeat_value: 'no-repeat',
+          size_value: '150%',
+          color_dark: '#0B0200',
+          color_medium: '#381D12',
+          color_light: '#817B7D',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
