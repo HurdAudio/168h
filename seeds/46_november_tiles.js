c2ea100f065bc7fc38857ccfef032ce213960ef5
Devin Hurd <hurdaudio@gmail.com>
2018-03-16 16:01:10 -0600

    update november tiles seeds

--- seeds/46_november_tiles.js
+++ seeds/46_november_tiles.js
@@ -410,6 +410,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#F8F8F8',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 30,
+          user_id: 1,
+          theme: 'Monochromatic',
+          type: 'saturday',
+          src_string: '../img/november_tiles/photo-728711.JPG',
+          repeat_value: 'no-repeat',
+          size_value: '135%',
+          color_dark: '#06070C',
+          color_medium: '#6D687E',
+          color_light: '#FEF6F4',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
