725a7b0a8246ff3a3c19f6c594b84db72fab7e46
Devin Hurd <hurdaudio@gmail.com>
2018-04-05 15:01:23 -0600

    update june tiles seeds

--- seeds/41_june_tiles.js
+++ seeds/41_june_tiles.js
@@ -564,6 +564,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#EEEFED',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 41,
+          user_id: 1,
+          theme: 'Light',
+          type: 'sunday',
+          src_string: '../img/june_tiles/orange-peel-oil-lamps-2140976_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '135%',
+          color_dark: '#000000',
+          color_medium: '#FD5E1E',
+          color_light: '#FCFFC1',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
