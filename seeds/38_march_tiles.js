aa428548ab9577d908dc7ef126dbcec6ea934941
Devin Hurd <hurdaudio@gmail.com>
2018-04-12 17:03:32 -0600

    update march tiles seeds

--- seeds/38_march_tiles.js
+++ seeds/38_march_tiles.js
@@ -634,6 +634,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FDFBE9',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 46,
+          user_id: 1,
+          theme: 'Glass',
+          type: 'sunday',
+          src_string: '../img/march_tiles/stained-glass-windows-2669333_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '135%',
+          color_dark: '#110C12',
+          color_medium: '#6E939D',
+          color_light: '#D9D5E4',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
