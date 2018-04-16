48d38caae74ed60ad2794ad673f78c593327c2a7
Devin Hurd <hurdaudio@gmail.com>
2018-04-16 14:09:33 -0600

    update july tiles seeds

--- seeds/42_july_tiles.js
+++ seeds/42_july_tiles.js
@@ -592,6 +592,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#EEE1F0',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 43,
+          user_id: 1,
+          theme: 'Steam',
+          type: 'week',
+          src_string: '../img/july_tiles/coffeesteam.jpeg',
+          repeat_value: 'no-repeat',
+          size_value: '152%',
+          color_dark: '#4A5656',
+          color_medium: '#6C7876',
+          color_light: '#EFEFEF',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
