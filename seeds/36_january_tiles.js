8d21baf412b3e6b815838de85a49455b687effd7
Devin Hurd <hurdaudio@gmail.com>
2018-04-16 14:47:52 -0600

    update january tiles seeds

--- seeds/36_january_tiles.js
+++ seeds/36_january_tiles.js
@@ -676,6 +676,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#F7FBFE',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 49,
+          user_id: 1,
+          theme: 'Ice',
+          type: 'friday',
+          src_string: '../img/january_tiles/ice-texture-32-p1040785.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '152%',
+          color_dark: '#0D0D0F',
+          color_medium: '#7E673F',
+          color_light: '#F0EBDD',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
