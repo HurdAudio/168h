ab0dbc9e716d499dc2b4eaa25c019da7b75090e8
Devin Hurd <hurdaudio@gmail.com>
2018-04-19 17:19:45 -0600

    update july tiles seeds

--- seeds/42_july_tiles.js
+++ seeds/42_july_tiles.js
@@ -606,6 +606,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#EFEFEF',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 44,
+          user_id: 1,
+          theme: 'Steam',
+          type: 'friday',
+          src_string: '../img/july_tiles/cumulus_1.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '140%',
+          color_dark: '#020214',
+          color_medium: '#A0928A',
+          color_light: '#F2F5FB',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
