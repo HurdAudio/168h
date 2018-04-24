a1def92fdabe50259e45eba216eb4d7da66f35d6
Devin Hurd <hurdaudio@gmail.com>
2018-04-24 16:45:26 -0600

    update may tiles seeds

--- seeds/40_may_tiles.js
+++ seeds/40_may_tiles.js
@@ -648,6 +648,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#43ED0E',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 47,
+          user_id: 1,
+          theme: 'Greenery',
+          type: 'holiday',
+          src_string: '../img/may_tiles/15729479691_65321ae83d_b.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '150%',
+          color_dark: '#0A0A0E',
+          color_medium: '#789464',
+          color_light: '#FCFBF5',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
