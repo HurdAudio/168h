4ebf97814e13158fbbed417d957eaa2ea4793d13
Devin Hurd <hurdaudio@gmail.com>
2018-06-02 13:23:29 -0600

    update december tiles seeds

--- seeds/47_december_tiles.js
+++ seeds/47_december_tiles.js
@@ -634,6 +634,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#BCC0CC',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 46,
+          user_id: 1,
+          theme: 'Snow',
+          type: 'sunday',
+          src_string: '../img/december_tiles/vineyard-636229_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '140%',
+          color_dark: '#232320',
+          color_medium: '#7B95AC',
+          color_light: '#F7F7F5',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
