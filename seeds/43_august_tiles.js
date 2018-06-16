ea3c36ee7b94e75297f049cf957563a562e848ae
Devin Hurd <hurdaudio@gmail.com>
2018-06-16 11:08:29 -0600

    update august tiles seeds

--- seeds/43_august_tiles.js
+++ seeds/43_august_tiles.js
@@ -704,6 +704,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#D4C3B9',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 51,
+          user_id: 1,
+          theme: 'Primary Colors',
+          type: 'sunday',
+          src_string: '../img/august_tiles/valentin-lacoste-432757-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '135%',
+          color_dark: '#040000',
+          color_medium: '#FECD00',
+          color_light: '#FEFFFF',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
