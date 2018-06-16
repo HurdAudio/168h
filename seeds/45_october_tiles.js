55311666e5cf2517fbe92fe0af132e9db1aa5de2
Devin Hurd <hurdaudio@gmail.com>
2018-06-16 10:57:33 -0600

    update october tiles seeds

--- seeds/45_october_tiles.js
+++ seeds/45_october_tiles.js
@@ -676,6 +676,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FDFEFE',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 49,
+          user_id: 1,
+          theme: 'Autumnal',
+          type: 'friday',
+          src_string: '../img/october_tiles/micah-hallahan-38148-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '153%',
+          color_dark: '#09092B',
+          color_medium: '#C67727',
+          color_light: '#F3DFBC',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
