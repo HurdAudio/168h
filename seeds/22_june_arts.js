8f37c51e358f3cc242261ef123801d49d3800b3d
Devin Hurd <hurdaudio@gmail.com>
2018-07-03 19:13:39 -0600

    update june arts seeds

--- seeds/22_june_arts.js
+++ seeds/22_june_arts.js
@@ -7204,6 +7204,106 @@ exports.seed = function(knex, Promise) {
           },
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 361,
+          user_id: 1,
+          theme: 'Photography',
+          img_path: './img/june/Old-Studebaker-small-1.jpg',
+          title: 'Old Studebaker',
+          artist: 'Steve Bingham',
+          year: '2013',
+          rule: {
+            "monday": [ 16, 26, 6, 14 ],
+            "tuesday": [ 15, 25, 5, 13 ],
+            "wednesday": [ 21, 1, 11, 17 ],
+            "thursday": [ 20, 30, 10, 18 ],
+            "friday": [ 19, 29, 9, 17 ],
+            "saturday": [ 18, 28, 8, 16 ],
+            "sunday": [ 27, 7, 17, 15 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 362,
+          user_id: 1,
+          theme: 'Photography',
+          img_path: './img/june/Colorado-mine-1.jpg',
+          title: 'Longfellow Mine, Colorado (On Red Mountain)',
+          artist: 'Steve Bingham',
+          year: 'xxi century',
+          rule: {
+            "monday": [ 27, 7, 17, 15 ],
+            "tuesday": [ 16, 26, 6, 14 ],
+            "wednesday": [ 15, 25, 5, 13 ],
+            "thursday": [ 21, 1, 11, 17 ],
+            "friday": [ 20, 30, 10, 18 ],
+            "saturday": [ 19, 29, 9, 17 ],
+            "sunday": [ 18, 28, 8, 16 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 363,
+          user_id: 1,
+          theme: 'Photography',
+          img_path: './img/june/Door-ghost.jpg',
+          title: 'Gone too Long - Sage, Wyoming I',
+          artist: 'Steve Bingham',
+          year: 'xxi century',
+          rule: {
+            "monday": [ 18, 28, 8, 16 ],
+            "tuesday": [ 27, 7, 17, 15 ],
+            "wednesday": [ 16, 26, 6, 14 ],
+            "thursday": [ 15, 25, 5, 13 ],
+            "friday": [ 21, 1, 11, 17 ],
+            "saturday": [ 20, 30, 10, 18 ],
+            "sunday": [ 19, 29, 9, 17 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 364,
+          user_id: 1,
+          theme: 'Photography',
+          img_path: './img/june/Underwood-Typewriter.jpg',
+          title: 'Underwood Typewriter - Stein, New Mexico',
+          artist: 'Steve Bingham',
+          year: 'xxi century',
+          rule: {
+            "monday": [ 20, 30, 10, 18 ],
+            "tuesday": [ 19, 29, 9, 17 ],
+            "wednesday": [ 28, 8, 18, 16 ],
+            "thursday": [ 17, 27, 7, 15 ],
+            "friday": [ 16, 26, 6, 14 ],
+            "saturday": [ 22, 2, 12, 18 ],
+            "sunday": [ 21, 1, 11, 19 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 365,
+          user_id: 1,
+          theme: 'Photography',
+          img_path: './img/june/_DSC2409.jpg',
+          title: 'Embudo, New Mexico - Phillips Route 66 Sign',
+          artist: 'Steve Bingham',
+          year: 'xxi century',
+          rule: {
+            "monday": [ 21, 1, 11, 19 ],
+            "tuesday": [ 20, 30, 10, 18 ],
+            "wednesday": [ 19, 29, 9, 17 ],
+            "thursday": [ 28, 8, 18, 16 ],
+            "friday": [ 17, 27, 7, 15 ],
+            "saturday": [ 16, 26, 6, 14 ],
+            "sunday": [ 22, 2, 12, 18 ]
+          },
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
