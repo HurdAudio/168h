3271245c396946fea07352d4636a14f0b4febaf8
Devin Hurd <hurdaudio@gmail.com>
2018-07-08 22:10:20 -0600

    update may tiles seeds

--- seeds/40_may_tiles.js
+++ seeds/40_may_tiles.js
@@ -788,6 +788,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FFFFFF',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 57,
+          user_id: 1,
+          theme: 'Greenery',
+          type: 'holiday',
+          src_string: '../img/may_tiles/juan-rojas-712924-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '145%',
+          color_dark: '#040200',
+          color_medium: '#E5B222',
+          color_light: '#FED698',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
