9b911286e1411a3dba145da7e53f81f081e336b2
Devin Hurd <hurdaudio@gmail.com>
2018-08-11 15:24:30 -0600

    update july tiles seeds

--- seeds/42_july_tiles.js
+++ seeds/42_july_tiles.js
@@ -774,6 +774,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#BABDC2',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 56,
+          user_id: 1,
+          theme: 'Steam',
+          type: 'sunday',
+          src_string: '../img/july_tiles/chantal-ole-669105-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '160%',
+          color_dark: '#010221',
+          color_medium: '#93489A',
+          color_light: '#E7936F',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
