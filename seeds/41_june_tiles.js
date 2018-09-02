26959eda2e707e618da002fe316aa781c509a0ba
Devin Hurd <hurdaudio@gmail.com>
2018-09-02 12:17:03 -0600

    update june tiles seeds

--- seeds/41_june_tiles.js
+++ seeds/41_june_tiles.js
@@ -802,6 +802,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FDFDFD',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 58,
+          user_id: 1,
+          theme: 'Light',
+          type: 'week',
+          src_string: '../img/june_tiles/jean-philippe-delberghe-667771-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '160%',
+          color_dark: '#0B0A0F',
+          color_medium: '#96A6C5',
+          color_light: '#F9EED0',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
