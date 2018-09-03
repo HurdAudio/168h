64e67b3602a1f4f8fde892d430fc6b28604fce43
Devin Hurd <hurdaudio@gmail.com>
2018-09-03 16:56:13 -0600

    update august tiles seeds

--- seeds/43_august_tiles.js
+++ seeds/43_august_tiles.js
@@ -788,6 +788,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#E1DAFC',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 57,
+          user_id: 1,
+          theme: 'Primary Colors',
+          type: 'holiday',
+          src_string: '../img/august_tiles/patrick-fore-389428-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '160%',
+          color_dark: '#232122',
+          color_medium: '#CF5078',
+          color_light: '#9DEEFA',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
