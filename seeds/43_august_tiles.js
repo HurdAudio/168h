578af7fb4305073fef83c9d68e61129adbff5908
Devin Hurd <hurdaudio@gmail.com>
2018-09-23 14:51:06 -0600

    update august tiles seeds

--- seeds/43_august_tiles.js
+++ seeds/43_august_tiles.js
@@ -816,6 +816,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#DFCB0C',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 59,
+          user_id: 1,
+          theme: 'Primary Colors',
+          type: 'friday',
+          src_string: '../img/august_tiles/ekamelev-688472-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '130%',
+          color_dark: '#141301',
+          color_medium: '#964100',
+          color_light: '#EAD000',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
