8e64e528ef64f18ee7ead857d357d0f8dc38aa84
Devin Hurd <hurdaudio@gmail.com>
2018-09-23 17:50:38 -0600

    update january tiles seeds

--- seeds/36_january_tiles.js
+++ seeds/36_january_tiles.js
@@ -914,6 +914,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#D0F1E6',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 66,
+          user_id: 1,
+          theme: 'Ice',
+          type: 'sunday',
+          src_string: '../img/january_tiles/john-bakator-571760-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '156%',
+          color_dark: '#020204',
+          color_medium: '#7F6E66',
+          color_light: '#DCE3ED',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
