e8527c98ffcce7fea67fabe35ed265f2debee22f
Devin Hurd <hurdaudio@gmail.com>
2018-09-23 17:45:07 -0600

    update february tiles seeds

--- seeds/37_february_tiles.js
+++ seeds/37_february_tiles.js
@@ -900,6 +900,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FCE4F3',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 65,
+          user_id: 1,
+          theme: 'Silk/Velvet',
+          type: 'saturday',
+          src_string: '../img/february_tiles/ivann-schlosser-396866-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '156%',
+          color_dark: '#0B2C4A',
+          color_medium: '#CFB081',
+          color_light: '#BFAC9D',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
