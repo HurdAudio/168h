5893e0502ded3e34bc1314ce64fb4d3500bd0803
Devin Hurd <hurdaudio@gmail.com>
2018-10-06 21:24:02 -0600

    update october tiles seeds

--- seeds/45_october_tiles.js
+++ seeds/45_october_tiles.js
@@ -816,6 +816,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#CDBD5B',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 59,
+          user_id: 1,
+          theme: 'Autumnal',
+          type: 'friday',
+          src_string: '../img/october_tiles/mihai-surdu-406935-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '155%',
+          color_dark: '#0E1313',
+          color_medium: '#AFC1D7',
+          color_light: '#FDFDFF',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
