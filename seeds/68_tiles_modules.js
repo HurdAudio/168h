134e75ed644bd24a3956956148937fec90623d7c
Devin Hurd <hurdaudio@gmail.com>
2018-10-27 17:46:49 -0600

    update tiles modules seeds

--- seeds/68_tiles_modules.js
+++ seeds/68_tiles_modules.js
@@ -559,6 +559,21 @@ exports.seed = function(knex, Promise) {
           public: true,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 38,
+          user_author_id: 1,
+          theme: 'wood',
+          type: 'sunday',
+          src_string: '../img/tiles/ron-whitaker-551781-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '160%',
+          color_dark: '#100604',
+          color_medium: '#BD99A3',
+          color_light: '#FFEE84',
+          public: true,
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
