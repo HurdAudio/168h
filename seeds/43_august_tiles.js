3ec369889283d1fe9316039fad0bf20591992dcd
Devin Hurd <hurdaudio@gmail.com>
2018-10-27 19:15:36 -0600

    update august tiles seeds

--- seeds/43_august_tiles.js
+++ seeds/43_august_tiles.js
@@ -872,6 +872,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#F3F6FB',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 63,
+          user_id: 1,
+          theme: 'Primary Colors',
+          type: 'week',
+          src_string: '../img/august_tiles/sharon-mccutcheon-645678-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '155%',
+          color_dark: '#06003A',
+          color_medium: '#CC0019',
+          color_light: '#E8F18C',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
