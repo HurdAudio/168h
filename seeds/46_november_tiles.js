5522cef21ab31c8abdf512c17d34d654acfab970
Devin Hurd <hurdaudio@gmail.com>
2018-11-02 18:56:43 -0600

    update november tiles seeds

--- seeds/46_november_tiles.js
+++ seeds/46_november_tiles.js
@@ -844,6 +844,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#CCFEFF',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 61,
+          user_id: 1,
+          theme: 'Monochromatic',
+          type: 'sunday',
+          src_string: '../img/november_tiles/federica-galli-445447-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '160%',
+          color_dark: '#04235A',
+          color_medium: '#0168E8',
+          color_light: '#C1EDFD',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
