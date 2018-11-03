e876b72c2ef7a09afa55bbd549b3ab473ef49af0
Devin Hurd <hurdaudio@gmail.com>
2018-11-02 19:01:06 -0600

    update october tiles seeds

--- seeds/45_october_tiles.js
+++ seeds/45_october_tiles.js
@@ -858,6 +858,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#FFEAE2',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 62,
+          user_id: 1,
+          theme: 'Autumnal',
+          type: 'holiday',
+          src_string: '../img/october_tiles/lampion-204067_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '155%',
+          color_dark: '#070805',
+          color_medium: '#A5342B',
+          color_light: '#FDFBB5',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
