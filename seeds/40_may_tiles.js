f1df03128fa85416bbe62a779fc0c8b21df58b70
Devin Hurd <hurdaudio@gmail.com>
2018-12-03 21:24:52 -0700

    update may tiles seeds

--- seeds/40_may_tiles.js
+++ seeds/40_may_tiles.js
@@ -984,6 +984,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#DAD3D8',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 71,
+          user_id: 1,
+          theme: 'Greenery',
+          type: 'sunday',
+          src_string: '../img/may_tiles/mark-kucharski-755757-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '100%',
+          color_dark: '#1E121C',
+          color_medium: '#3D9E3A',
+          color_light: '#EAFA84',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
