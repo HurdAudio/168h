65f0d2a34d8922955239cd1a950a52cb681ee343
Devin Hurd <hurdaudio@gmail.com>
2018-12-09 18:25:46 -0700

    update tiles seeds

--- seeds/68_tiles_modules.js
+++ seeds/68_tiles_modules.js
@@ -649,6 +649,21 @@ exports.seed = function(knex, Promise) {
           public: true,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 44,
+          user_author_id: 1,
+          theme: 'wood',
+          type: 'holiday',
+          src_string: '../img/tiles/tim-easley-317908-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '160%',
+          color_dark: '#150001',
+          color_medium: '#E65101',
+          color_light: '#FDBC0C',
+          public: true,
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
