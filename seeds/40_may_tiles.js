718b1f99ca9d6eef585cf93d4a6f2bfc9a49522c
Devin Hurd <hurdaudio@gmail.com>
2019-04-20 08:19:37 -0600

    update may tiles seeds

--- seeds/40_may_tiles.js
+++ seeds/40_may_tiles.js
@@ -1138,6 +1138,20 @@ exports.seed = function(knex, Promise) {
           color_light: '#BCC1C7',
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 82,
+          user_id: 1,
+          theme: 'Greenery',
+          type: 'holiday',
+          src_string: 'https://s3.amazonaws.com/hours168-hurdaudio/img/may_tiles/strawberry-260687_1920.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '140%',
+          color_dark: '#040A00',
+          color_medium: '#D02914',
+          color_light: '#E7E9D1',
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
