2042dd25c1d594f8a0fa4b708335b62d4d75b412
Devin Hurd <hurdaudio@gmail.com>
2019-04-29 05:40:51 -0600

    update tiles seeds

--- seeds/68_tiles_modules.js
+++ seeds/68_tiles_modules.js
@@ -829,6 +829,21 @@ exports.seed = function(knex, Promise) {
           public: true,
           created_at: new Date('2017-07-20T13:44:00.000Z'),
           updated_at: new Date('2017-07-20T13:44:00.000Z')
+        },
+        {
+          id: 56,
+          user_author_id: 1,
+          theme: 'wood',
+          type: 'friday',
+          src_string: 'https://s3.amazonaws.com/hours168-hurdaudio/img/tiles/valentin-salja-1379305-unsplash.jpg',
+          repeat_value: 'no-repeat',
+          size_value: '100%',
+          color_dark: '#030000',
+          color_medium: '#3F3631',
+          color_light: '#C4A186',
+          public: true,
+          created_at: new Date('2017-07-20T13:44:00.000Z'),
+          updated_at: new Date('2017-07-20T13:44:00.000Z')
         }
       ]);
     }).then(() => {
