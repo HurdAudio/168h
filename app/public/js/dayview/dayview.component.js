5a3bdd0f7c7cf747d8c45de4e55a97584027a44c
Devin Hurd <hurdaudio@gmail.com>
2019-05-27 22:19:43 -0600

    observances no longer one day off on server side

--- app/public/js/dayview/dayview.component.js
+++ app/public/js/dayview/dayview.component.js
@@ -2510,7 +2510,7 @@
         // alert('here');
         let indexArt = vm.arts.length;
         let indexMusic = vm.musics.length;
-        let userTimezoneOffset = viewDate.getTimezoneOffset() * 60000;
+        // let userTimezoneOffset = viewDate.getTimezoneOffset() * 60000;
         vm.observances = [];
         let observancesPane = document.getElementById('observancesPane');
         $http.get(`/observancesbyuser/${currentUserId}`)
@@ -2519,8 +2519,8 @@
           let observanceDate;
           for (let i = 0; i < userObservances.length; i++) {
             observanceDate = new Date(userObservances[i].day_of);
-            observanceDate = new Date(observanceDate.getTime() + userTimezoneOffset);
-            if ((viewDate.getMonth() === observanceDate.getMonth()) && (viewDate.getDate() === (observanceDate.getDate()))) {
+            // observanceDate = new Date(observanceDate.getTime() + userTimezoneOffset);
+            if ((viewDate.getMonth() === observanceDate.getUTCMonth()) && (viewDate.getDate() === (observanceDate.getUTCDate()))) {
               vm.observances.push(userObservances[i]);
             }
           }
